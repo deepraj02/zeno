31791109e429ae9a2a3f92460cb99d594adc7027
Deepraj Baidya <deeprajbaidya@gmail.com>
2025-09-17 17:57:46 +0530

    🚀 project config with vg_cli

---
+++ lib/zeno.dart
@@ -0,0 +1,10 @@
+/// zeno, A hot reload utility for Dart Applications
+///
+/// ```sh
+/// # activate zeno
+/// dart pub global activate zeno
+///
+/// # see usage
+/// zeno --help
+/// ```
+library;
