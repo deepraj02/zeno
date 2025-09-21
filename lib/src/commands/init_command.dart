f9cfbb80a3c771b65eb31cbd8be36e240ff636e0
Deepraj Baidya <deeprajbaidya@gmail.com>
2025-09-21 17:45:35 +0530

    ✨ init command and config file generation

--- lib/src/commands/init_command.dart
+++ lib/src/commands/init_command.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:args/command_runner.dart';
 import 'package:mason_logger/mason_logger.dart';
+import 'package:zeno/src/utils/path_utils.dart';
 
 /// Command to initialize a new Zeno configuration file.
 class InitCommand extends Command<int> {
@@ -18,8 +21,32 @@ class InitCommand extends Command<int> {
 
   @override
   Future<int> run() async {
-    const output = 'Init Running';
-    _logger.info(output);
-    return ExitCode.success.code;
+    final configFile = File(_configFileName);
+
+    if (await PathUtils.shouldSkipInitialization(configFile, _logger)) {
+      return ExitCode.success.code;
+    }
+
+    try {
+      await PathUtils.createConfigurationFile(
+        configFile,
+        _logger,
+        _configFileName,
+      );
+      _displaySuccessMessage();
+      return ExitCode.success.code;
+    } catch (error) {
+      _logger.err('Failed to create configuration file: $error');
+      return ExitCode.software.code;
+    }
+  }
+
+  void _displaySuccessMessage() {
+    _logger
+      ..info('Edit the configuration file to specify:')
+      ..info('  • build.cmd: Your build command')
+      ..info('  • build.bin: Your output binary path')
+      ..info('')
+      ..info('Run "zeno" to start hot reloading');
   }
 }
